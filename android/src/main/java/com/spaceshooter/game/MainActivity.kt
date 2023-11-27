72f27005aed320e896872a8b8e8d1eb522ad2511
miikkaniska <78212251+miikkaniska@users.noreply.github.com>
2023-11-28 01:16:56 +0200

    Placeholder music for testing. Dialog box when trying to exit game.

--- android/src/main/java/com/spaceshooter/game/MainActivity.kt
+++ android/src/main/java/com/spaceshooter/game/MainActivity.kt
@@ -1,23 +1,46 @@
 package com.spaceshooter.game
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.spaceshooter.game.android.AndroidLauncher
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var btnGame: Button
+    private lateinit var btnHighscore: Button
+    private lateinit var btnSettings: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnGame = findViewById<Button>(R.id.btn_game)
-        val btnHighscore = findViewById<Button>(R.id.btn_highscore)
-        val btnSettings = findViewById<Button>(R.id.btn_settings)
 
+        btnGame = findViewById(R.id.btn_game)
+        btnHighscore = findViewById(R.id.btn_highscore)
+        btnSettings = findViewById(R.id.btn_settings)
+
+        // Assuming you have a file named "test_music.mp3" in the res/raw directory
+        val resourceId = resources.getIdentifier("test_music", "raw", packageName)
+        mediaPlayer = MediaPlayer.create(this, resourceId)
+        mediaPlayer.start()
 
         btnGame.setOnClickListener {
+            // Start the game activity
             val intent = Intent(this, AndroidLauncher::class.java)
+            mediaPlayer.stop()
+            finish()
             startActivity(intent)
         }
+
     }
+    override fun onBackPressed() {
+        mediaPlayer.release()
+        finish()
+    }
+
 }
--- android/src/main/kotlin/com/spaceshooter/game/android/AndroidLauncher.kt
+++ android/src/main/kotlin/com/spaceshooter/game/android/AndroidLauncher.kt
@@ -1,10 +1,13 @@
 package com.spaceshooter.game.android
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.spaceshooter.game.Main
+import com.spaceshooter.game.MainActivity
 
 /** Launches the Android application. */
 class AndroidLauncher : AndroidApplication() {
@@ -15,4 +18,29 @@ class AndroidLauncher : AndroidApplication() {
             useImmersiveMode = true // Recommended, but not required.
         })
     }
+    override fun onBackPressed() {
+        showExitConfirmationDialog()
+    }
+
+    private fun showExitConfirmationDialog(){
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("Exit")
+            .setMessage("Are you sure you want exit game")
+            .setPositiveButton("Yes") { _, _ ->
+                // User confirmed, so allow the default behavior
+                val intent = Intent(this, MainActivity::class.java)
+                finish()
+                startActivity(intent)
+
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                // User canceled, so dismiss the dialog
+                dialog.dismiss()
+            }
+            .create()
+
+        alertDialog.show()
+    }
+
+
 }
