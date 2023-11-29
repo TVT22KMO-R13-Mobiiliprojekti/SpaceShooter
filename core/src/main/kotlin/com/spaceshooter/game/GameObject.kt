97136dce4cfa6023c14f8ab0fff33201417578c8
LassiVaisanen <t1vala02@students.oamk.fi>
2023-11-29 19:33:26 +0200

    Flip enemy texture

--- core/src/main/kotlin/com/spaceshooter/game/GameObject.kt
+++ core/src/main/kotlin/com/spaceshooter/game/GameObject.kt
@@ -53,4 +53,9 @@ open class GameObject(var position: Vector2 = Vector2(0.0f, 0.0f), var size: Vec
     {
         return speed
     }
+
+    fun checkCollision(gameObject: GameObject)
+    {
+        
+    }
 }
