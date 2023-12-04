019686dc536218269980bb14d930430ac133b2de
Matti Nurmela <t1numa00@students.oamk.fi>
2023-12-04 15:38:05 +0200

    hud updates for healthbar

--- core/src/main/kotlin/com/spaceshooter/game/Hud.kt
+++ core/src/main/kotlin/com/spaceshooter/game/Hud.kt
@@ -5,15 +5,18 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-
 class Hud(val batch: SpriteBatch) {
     private val font: BitmapFont = BitmapFont()
     val fontScale = 3f
-
-    private val healthBarTexture: Texture = Texture("healthBar.png")
-    private val maxHealthWidth = 1000f //
-    private val healthBarHeight = 800f
-    private var playerHealth = 100f //
+    private var playerHealth = 100f // Max health 100
+    private val healthBarFrameTexture: Texture = Texture("healthBar.png") // Frame picture
+    private val healthContentTexture: Texture = Texture("healthContent.png") // Healthbar content picture
+    private val healthFrameWidth = 2400f // Frame Width
+    private val healthFrameHeight = 800f // Frame Height
+    private val contentWidth = 450f * (playerHealth / 100f)// Health width
+    private val contentHeight = 40f // Health height
+    private val frameX = -500f
+    private val frameY = Gdx.graphics.height - 500f
     var score = 0
 
     fun draw() {
@@ -21,13 +24,22 @@ class Hud(val batch: SpriteBatch) {
         font.data.setScale(fontScale)
         font.draw(batch, "Score: $score", 20f, Gdx.graphics.height - 20f)
 
-        // Draw healthbar
+        // Draw healthbar frame first
         batch.draw(
-            healthBarTexture,
-            -100f,
-            Gdx.graphics.height - 500f,
-            maxHealthWidth * (playerHealth / 100f),
-            healthBarHeight
+            healthBarFrameTexture,
+            frameX,
+            frameY,
+            healthFrameWidth,
+            healthFrameHeight
+        )
+
+//         Draw healthbar content within the frame
+        batch.draw(
+            healthContentTexture,
+            frameX + 950f,
+            frameY + 425,
+            contentWidth,
+            contentHeight
         )
     }
 
@@ -37,7 +49,7 @@ class Hud(val batch: SpriteBatch) {
 
     fun dispose() {
         font.dispose()
-        healthBarTexture.dispose()
+        healthBarFrameTexture.dispose()
+        healthContentTexture.dispose()
     }
 }
-
