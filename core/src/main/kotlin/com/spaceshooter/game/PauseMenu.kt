b4be7cc8cfc5dc6dbb27586c07d07afd2d224db2
Matti Nurmela <t1numa00@students.oamk.fi>
2023-12-14 00:48:37 +0200

    PauseMenu dialog buttons and texts scaled

--- core/src/main/kotlin/com/spaceshooter/game/PauseMenu.kt
+++ core/src/main/kotlin/com/spaceshooter/game/PauseMenu.kt
@@ -1,24 +1,37 @@
 package com.spaceshooter.game
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 
-
 class PauseMenu(private val skin: Skin, private val stage: Stage) {
-
-
+    private val fontScale = 3.0f
     private val dialog: Dialog = createMenuDialog()
 
     private fun createMenuDialog(): Dialog {
-        val dialog = Dialog("Menu", skin)
+        val titleLabel = Label("Pause menu", skin).apply {
+            setFontScale(fontScale)
+        }
+        val dialog = Dialog("", skin).apply {
+            contentTable.add(titleLabel).padTop(-200f).padBottom(50f).row()
+        }
+
+        val buttonStyle = skin.get(TextButton.TextButtonStyle::class.java)
+        val scaledFont = BitmapFont().apply {
+            data.setScale(fontScale) // Set font scale
+        }
+
+        buttonStyle.font = scaledFont // Set scaled font to style
 
         val quitButton = TextButton("Quit", skin)
+        quitButton.style = buttonStyle
         quitButton.addListener {
             // Stop game
-            //Gdx.app.exit()
+            // Gdx.app.exit()
             true
         }
         val continueButton = TextButton("Continue", skin)
@@ -34,7 +47,6 @@ class PauseMenu(private val skin: Skin, private val stage: Stage) {
 
         val sfxButton = TextButton("SFX On/Off", skin)
         sfxButton.addListener {
-            // Add functionality
             true
         }
 
@@ -44,8 +56,8 @@ class PauseMenu(private val skin: Skin, private val stage: Stage) {
         dialog.contentTable.add(sfxButton).row()
 
         dialog.isMovable = false
-        dialog.setSize(1000f, 600f)
-        dialog.setPosition(550f,220f)
+        dialog.setSize(900f, 600f)
+        dialog.setPosition(700f,220f)
 
         return dialog
     }
